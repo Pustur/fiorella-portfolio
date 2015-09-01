5c7fb375ab9b5826a3116513113543cd96e828ca
Loris Bettazza <loriteck@yahoo.it>
2015-09-02 01:39:29 +0200

    Replaced default alert with sweetalert when deleting an element

--- resources/views/admin/works/show.blade.php
+++ resources/views/admin/works/show.blade.php
@@ -1,5 +1,9 @@
 @extends('admin', [$title = $work->title])
 
+@section('meta')
+	<link rel="stylesheet" href="/css/sweetalert.min.css">
+@stop
+
 @section('content')
 	@include('partials.breadcrumbs', $breadcrumbs = [
 		['link' => route('admin.index'),       'text' => 'Sezione amministrativa'],
@@ -28,7 +32,27 @@
 	<hr>
 
 	<a class="button" href="{{ route('admin.works.edit', $work->slug) }}">Modifica</a>
-	{!! Form::open(['method' => 'DELETE', 'route' => ['admin.works.destroy', $work->slug], 'class' => 'inline', 'onsubmit' => 'return confirm("Sei sicuro di voler eliminare questo elemento?");']); !!}
-	{!! Form::submit('Elimina', ['class' => 'button button-danger']); !!}
+	{!! Form::open(['method' => 'DELETE', 'route' => ['admin.works.destroy', $work->slug], 'class' => 'inline', 'id' => 'delete-form']); !!}
+	{!! Form::submit('Elimina', ['class' => 'button button-danger', 'onclick' => 'return deleteElement();']); !!}
 	{!! Form::close(); !!}
+@stop
+
+@section('footer')
+	<script src="/js/sweetalert.min.js"></script>
+	<script>
+		function deleteElement(){
+			swal({
+				title: 'Sei sicuro?',
+				text: 'Una volta eliminato questo elemento non sarà possibile recuperarlo!',
+				type: 'warning',
+				showCancelButton: true,
+				cancelButtonText: 'Indietro',
+				confirmButtonColor: '#b94744',
+				confirmButtonText: 'Sì, eliminalo'
+			}, function(){
+				document.getElementById('delete-form').submit();
+			});
+			return false;
+		}
+	</script>
 @stop
