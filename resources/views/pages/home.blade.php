2461b1d151e98ec007efeb71e1f21185832226db
Loris Bettazza <loriteck@yahoo.it>
2015-09-13 15:17:46 +0200

    Added lightbox plugin
    Added lightbox functionality to the homepage with the "featherlight.js"
    plugin

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,16 @@
 @extends('master', [$title = 'Home'])
 
 @section('content')
-    <div class="container">
-        <h1 class="home-title">Fiorella's Website</h1>
-    </div>
+    <header>
+        <div class="container">
+            <h1 class="home-title">Fiorella's Website</h1>
+        </div>
+    </header>
     <section class="gallery">
         @if($works->count())
             <div class="clearfix container" data-columns>
-                @foreach($works as $work)
-                    <a class="work" href="#">
+                @foreach($works as $i => $work)
+                    <a class="work" href="#" data-featherlight="#lightbox-{{ $i }}">
                         <img src="/img/works/thumbnail-{{ $work->image }}" alt="{{ $work->title }}">
                         <div class="work-overlay">
                             <h2>{{ $work->title }}</h2>
@@ -21,8 +23,22 @@
             <p class="no-margin text-center">Spiacenti, nessun lavoro trovato :(</p>
         @endif
     </section>
+    <section class="lightboxes">
+        @foreach($works as $i => $work)
+            <div id="lightbox-{{ $i }}">
+                <a href="/img/works/{{ $work->image }}" target="_blank">
+                    <img src="/img/works/{{ $work->image }}" alt="{{ $work->title }}">
+                </a>
+                <h2>{{ $work->title }}</h2>
+                <p>Tecnica usata: <strong>{{ $work->technique->name }}</strong></p>
+                <p>Dimensioni: <strong>{{ $work->size }}</strong></p>
+            </div>
+        @endforeach
+    </section>
 @stop
 
 @section('footer')
     <script src="/js/salvattore.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="/js/featherlight.min.js"></script>
 @stop
