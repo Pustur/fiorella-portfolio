d3c48d7d7a2ccfde17f10600058fc85854942d46
Loris Bettazza <loris.bettazza@gmail.com>
2022-07-09 23:56:43 +0200

    Remove google analytics
    Closes #4

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -52,15 +52,6 @@
 
 @section('meta')
     <meta name="description" content="Fiorella Giuliani Lanini è un'artista e pittrice, presenta le sue opere in questo spazio online.">
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-77566858-1', 'auto');
-        ga('send', 'pageview');
-    </script>
 @stop
 
 @section('content')
